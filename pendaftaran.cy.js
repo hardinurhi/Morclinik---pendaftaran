4a35ce2f130a74cb4d96e869fc8a936f83102174
hardinurhi <153154850+hardinurhi@users.noreply.github.com>
2023-12-17 18:39:48 +0700

    Update pendaftaranpasienlama.cy.js

--- pendaftaran.cy.js
+++ pendaftaran.cy.js
@@ -1,52 +1,45 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  // it('passes', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
 
   /* ==== Test Created with Cypress Studio ==== */
-  it('pasien baru', function() {
+  it('PasienLama', function() {
     /* ==== Generated with Cypress Studio ==== */
-    cy.visit('https://dev.morclinic.id/home');
+    cy.visit('dev.morclinic.id');
+    /* ==== End Cypress Studio ==== */
     cy.get('#username').type('Ro01');
     cy.get('#password').type('12345678');
-    cy.get('.btn-dark').click();
-    cy.get('.hover > .waves-effect > .site-menu-title').click();
-    cy.get('.ml-auto > .btn').click();
-    cy.get('.font-weight-500').click();
-    cy.get(':nth-child(3) > :nth-child(9) > #pilih-pasien').click();
-    cy.get('#no_telp').type('089506549544');
-    cy.get('#nama_pasien').type('GITA ');
-    cy.get('#nama_pasien_tengah').type('cahya');
-    cy.get('#nama_pasien_belakang').type('rembulan');
-    cy.get('#nik').type('1234567890123457');
-    cy.get('#tempat_lahir').type('bandung');
-    cy.get('#tanggal_lahir').click();
-    cy.get(':nth-child(2) > .today').click();
-    cy.get('#tanggal_lahir').type('07-12-1997');
-    cy.get(':nth-child(2) > .col-md-7').click();
-    cy.get('#select2-provinsi-container').click();
-    cy.get('#select2-kabupaten-container > .select2-selection__placeholder').click();
-    cy.get('.select2-search__field').type('sleman');
-    cy.get('#select2-kecamatan-container').click();
-    cy.get('#select2-kelurahan-container').click();
-    cy.get('#select2-gol_darah-container > .select2-selection__placeholder').click();
-    cy.get('#select2-agama-container > .select2-selection__placeholder').click();
-    cy.get(':nth-child(16) > .col-md-9 > .btn-group > .btn > .filter-option').click();
-    cy.get(':nth-child(16) > .col-md-9 > .btn-group > .open > .dropdown-menu > [data-original-index="2"] > a > .text').click();
-    cy.get('#select2-id_suku-container > .select2-selection__placeholder').click();
-    cy.get('.select2-search__field').type('jawa');
-    cy.get('#select2-status_perkawinan-container > .select2-selection__placeholder').click();
-    cy.get('#select2-id_pekerjaan-container > .select2-selection__placeholder').click();
-    cy.get('#select2-id_pendidikan-container').click();
-    cy.get('#kontak_penanggung_jawab').type('089506549578');
-    cy.get('#alamat_penanggung_jawab').click();
-    cy.get('#select2-hubungan_keluarga-container > .select2-selection__placeholder').click();
-    cy.get(':nth-child(1) > .col-md-9 > .btn-group > .btn > .filter-option').click();
-    cy.get(':nth-child(1) > .col-md-9 > .btn-group > .open > .dropdown-menu > .selected > a > .text').click();
-    cy.get('#select2-unit_umum-container').click();
-    cy.get('#select2-id_jadwal_klinik-container > .select2-selection__placeholder').click();
-    cy.get('#ladda-button-pendaftaran').click();
-    cy.get('.confirm').click();
-    /* ==== End Cypress Studio ==== */
-  });
+    cy.get('button[type=submit]').contains('Sign In').click();
+    cy.get('li a span[data-original-title="Pendaftaran Pasien"]').click();
+    cy.get('button[type=button]').contains('Daftar Pasien').click();
+    cy.get('button[type=button]').contains('Cari Pasien').click();    
+    cy.get('input#norm_pencarian').type('8').click();
+    cy.get('button[type=submit]').contains('Cari').click();
+    cy.get('button[type=button][data-norm=8]').click();    
+    // cy.get('input#nama_pasien_tengah').type('Nama Tengah');
+    // cy.get('input#nama_pasien_belakang').type('Nama Belakang');
+    // cy.get('input#nik').type('33750201010011118');
+    // cy.get('input#tempat_lahir').type('Dirumah');
+    // cy.get('input#tanggal_lahir').type('01/01/2000');
+    // cy.get('textarea#alamat').type('Nama Alamat Lengkap', {force:true});
+    // cy.get('select#provinsi').select('Jawa Tengah', {force:true}).focus();
+    // cy.get('select#kabupaten').select('Pekalongan', {force:true}).focus();
+    // cy.get('select#kecamatan').select('Pekalongan Timur', {force:true}).focus();
+    // cy.get('select#kelurahan').select('Kauman', {force:true}).focus();
+    // cy.get('select#gol_darah').select('A', {force:true}).focus();
+    // cy.get('select#agama').select('Islam', {force:true}).focus();
+    // cy.get('select#jenis_kelamin').select('Laki-laki', {force:true}).focus();
+    // cy.get('select#id_suku').select('JAWA', {force:true}).focus();
+    // cy.get('select#status_perkawinan').select('Kawin', {force:true}).focus();
+    // cy.get('select#id_pekerjaan').select('PNS', {force:true}).focus();
+    // cy.get('select#id_pendidikan').select('S1', {force:true}).focus();
+    cy.get('input[type=radio]#radio_rencana_pembayaran1').check();
+    cy.get('input[type=checkbox]#status_penanggungjawab').check();
+    cy.get('select#layanan').select('Pemeriksaan Umum', {force:true}).focus();
+    cy.get('select#unit_umum').select('POLI UMUM', {force:true}).focus();
+    cy.get('select#id_jadwal_klinik').select('Dr. dr. Bambang Satoto,Sp.Rad (K).M.Kes - Minggu(08:00:00 - 20:00:00) - POLI UMUM - 1000', {force:true}).focus();
+    cy.get('button[type=submit]').contains('Daftar').click();
+    cy.get('.sweet-alert button[tabindex=1]').contains('Daftar Pasien').click();
+  });  
 })
